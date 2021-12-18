cbf81ea2d323a474ee94914592f18383b8670143
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-12-18 14:37:01 +0300

    done for the lesson 2

--- input/config.js
+++ input/config.js
@@ -3,16 +3,38 @@ const dir = __dirname;
 const width = 1000;
 const height = 1000;
 
+const rarity = [
+  { key: "", val: "original" },
+  { key: "_r", val: "rare" },
+  { key: "_sr", val: "super rare" },
+];
+
+const addRarity = (_str) => {
+  let itemRarity;
+  rarity.forEach((r) => {
+    itemRarity = r.val;
+  });
+  return itemRarity;
+};
+
+const cleanName = (_str) => {
+  let name = _str.slice(0, -4);
+  rarity.forEach((r) => {
+    name = name.replace(r.key, "");
+  });
+  return name;
+};
+
 const getElements = (path) => {
   return fs
     .readdirSync(path)
     .filter((item) => !/(^|\/)\.[^|\/\.]/g.test(item))
     .map((i, index) => {
       return {
-        // id: index + 1,
-        // name: cleanName(i),
+        id: index + 1,
+        name: cleanName(i),
         fileName: i,
-        // rarity: addRarity(i),
+        rarity: addRarity(i),
       };
     });
 };
