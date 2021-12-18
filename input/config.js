0ea2cd5c2159790abe41abf4c997815dedd10b9d
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-12-18 13:37:40 +0300

    add config js

---
+++ input/config.js
@@ -0,0 +1,61 @@
+const dir = __dirname;
+const width = 1000;
+const height = 1000;
+const layers = [
+  {
+    id: 1,
+    name: "background",
+    location: `${dir}/background/ `,
+    elements: [],
+    position: { x: 0, y: 0 },
+    size: { width: width, height: height },
+  },
+  {
+    id: 1,
+    name: "ball",
+    location: `${dir}/ball/ `,
+    elements: [],
+    position: { x: 0, y: 0 },
+    size: { width: width, height: height },
+  },
+  {
+    id: 1,
+    name: "eye color",
+    location: `${dir}/eye color/ `,
+    elements: [],
+    position: { x: 0, y: 0 },
+    size: { width: width, height: height },
+  },
+  {
+    id: 1,
+    name: "iris",
+    location: `${dir}/iris/ `,
+    elements: [],
+    position: { x: 0, y: 0 },
+    size: { width: width, height: height },
+  },
+  {
+    id: 1,
+    name: "shine",
+    location: `${dir}/shine/ `,
+    elements: [],
+    position: { x: 0, y: 0 },
+    size: { width: width, height: height },
+  },
+  {
+    id: 1,
+    name: "bottom lid",
+    location: `${dir}/bottom lid/ `,
+    elements: [],
+    position: { x: 0, y: 0 },
+    size: { width: width, height: height },
+  },
+  {
+    id: 1,
+    name: "top lid",
+    location: `${dir}/top lid/ `,
+    elements: [],
+    position: { x: 0, y: 0 },
+    size: { width: width, height: height },
+  },
+];
