57ae143b8f34651a695c0be4f543edcaef0a9e44
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-12-18 15:56:19 +0300

    about to be done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const fs = require("fs");
 const myArgs = process.argv.slice(2);
 const { createCanvas, loadImage } = require("canvas");
 const { layers, width, height } = require("./input/config.js");
+const { decode } = require("punycode");
 //parameters: height and width
 const canvas = createCanvas(width, height);
 const ctx = canvas.getContext("2d");
@@ -20,16 +21,35 @@ const saveLayer = (_canvas, _edition) => {
   console.log("image created");
 };
 
-const addMetadata = (_element, _layer) => {
+const addMetadata = (_edition) => {
   let dateTime = Date.now();
 
   let tempMetadata = {
-    hash: "",
+    hash: hash.join(""),
     decodedHash: [],
-    edition: "",
+    edition: _edition,
     date: dateTime,
-    attributes: [],
+    attributes: attributes,
   };
+  metadata.push(tempMetadata);
+  //clearing
+  attributes = [];
+  hash = [];
+  decodedHash = [];
+};
+
+const addAttributes = (_element, _layer) => {
+  let tempAttr = {
+    id: _element.id,
+    layer: _layer.name,
+    name: _element.name,
+    rarity: _element.rarity,
+  };
+
+  attributes.push(tempAttr);
+  hash.push(_layer.id);
+  hash.push(_element.id);
+  decodedHash.push({ [_layer.id]: _element.id });
 };
 
 const drawLayer = async (_layer, _edition) => {
@@ -37,7 +57,7 @@ const drawLayer = async (_layer, _edition) => {
   let element =
     _layer.elements[Math.floor(Math.random() * _layer.elements.length)];
   // console.log(element);
-  addMetadata(element, _layer);
+  addAttributes(element, _layer);
   const image = await loadImage(`${_layer.location}${element.fileName}`);
   //   ctx.drawImage(img, x, y, width, height);
   ctx.drawImage(
@@ -57,5 +77,11 @@ for (let i = 1; i <= edition; i++) {
   layers.forEach((layer) => {
     drawLayer(layer, i);
   });
+  addMetadata();
   console.log("creating edition" + i);
 }
+
+fs.readFile("./output/_metadata.json", (err, data) => {
+  if (err) throw err;
+  fs.writeFileSync("./output/_metadata.json", JSON.stringify(metadata));
+});
