63ee1a8b2b3938331e39c19945fb227fda6b5928
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-12-18 15:21:18 +0300

    playing with indexjs

--- index.js
+++ index.js
@@ -4,18 +4,36 @@ const { createCanvas, loadImage } = require("canvas");
 const canvas = createCanvas(1000, 1000);
 const ctx = canvas.getContext("2d");
 const { layers, width, height } = require("./input/config.js");
+const edition = 1;
 
 const saveLayer = (_canvas) => {
   fs.writeFileSync("./newImage.png", _canvas.toBuffer("image/png"));
   console.log("image created");
 };
 
-const drawLayer = async () => {
-  const image = await loadImage("./eye_ball.png");
-  //   console.log(image);
+const drawLayer = async (_layer, _edition) => {
+  //get a random element
+  let element =
+    _layer.elements[Math.floor(Math.random() * _layer.elements.length)];
+  console.log(element);
+  const image = await loadImage(`${_layer.location}${element.fileName}`);
   //   ctx.drawImage(img, x, y, width, height);
-  ctx.drawImage(image, 0, 0, width, height);
-  console.log("ran");
-  saveLayer(canvas);
+  ctx.drawImage(
+    image,
+    _layer.position.x,
+    _layer.position.y,
+    _layer.size.width,
+    _layer.size.height
+  );
+  console.log(
+    `I created the ${_layer.name} layer, and choose element ${element.name}`
+  );
+  // saveLayer(canvas);
 };
-drawLayer();
+
+for (let i = 1; i <= edition; i++) {
+  layers.forEach((layer) => {
+    drawLayer(layer, i);
+  });
+  console.log("creating edition" + i);
+}
