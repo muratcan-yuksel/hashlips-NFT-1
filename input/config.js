5b5027a1483ee229ece519b8631607a0e8b2e9e6
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-12-18 13:54:20 +0300

    add get elements function

--- input/config.js
+++ input/config.js
@@ -1,60 +1,76 @@
+const fs = require("fs");
 const dir = __dirname;
 const width = 1000;
 const height = 1000;
+
+const getElements = (path) => {
+  return fs
+    .readdirSync(path)
+    .filter((item) => !/(^|\/)\.[^|\/\.]/g.test(item))
+    .map((i, index) => {
+      return {
+        id: index + 1,
+        name: cancelAnimationFrame(i),
+        fileName: i,
+        rarity: addRarity(i),
+      };
+    });
+};
+
 const layers = [
   {
     id: 1,
     name: "background",
     location: `${dir}/background/ `,
-    elements: [],
+    elements: getElements(`${dir}/background/ `),
     position: { x: 0, y: 0 },
     size: { width: width, height: height },
   },
   {
-    id: 1,
+    id: 2,
     name: "ball",
     location: `${dir}/ball/ `,
-    elements: [],
+    elements: getElements(`${dir}/ball/ `),
     position: { x: 0, y: 0 },
     size: { width: width, height: height },
   },
   {
-    id: 1,
+    id: 3,
     name: "eye color",
     location: `${dir}/eye color/ `,
-    elements: [],
+    elements: getElements(`${dir}/eye color/ `),
     position: { x: 0, y: 0 },
     size: { width: width, height: height },
   },
   {
-    id: 1,
+    id: 4,
     name: "iris",
     location: `${dir}/iris/ `,
-    elements: [],
+    elements: getElements(`${dir}/iris/ `),
     position: { x: 0, y: 0 },
     size: { width: width, height: height },
   },
   {
-    id: 1,
+    id: 5,
     name: "shine",
     location: `${dir}/shine/ `,
-    elements: [],
+    elements: getElements(`${dir}/shine/ `),
     position: { x: 0, y: 0 },
     size: { width: width, height: height },
   },
   {
-    id: 1,
+    id: 6,
     name: "bottom lid",
     location: `${dir}/bottom lid/ `,
-    elements: [],
+    elements: getElements(`${dir}/bottom lid/ `),
     position: { x: 0, y: 0 },
     size: { width: width, height: height },
   },
   {
-    id: 1,
+    id: 7,
     name: "top lid",
     location: `${dir}/top lid/ `,
-    elements: [],
+    elements: getElements(`${dir}/top lid/ `),
     position: { x: 0, y: 0 },
     size: { width: width, height: height },
   },
